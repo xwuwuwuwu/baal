e567d659d45e02ef5c37e01eceee15af2670a40a
yanzhangwu <yanzhangwu@163.com>
2020-05-22 10:20:23 +0800

    测试

--- src/FunctionApp.Api/Function1.cs
+++ src/FunctionApp.Api/Function1.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace FunctionApp
+namespace FunctionApp.Api
 {
     public static class Function1
     {
